61100749aa81049e66796ee41f5b15354360c0be
Rodrigo nonose <rodrigo.nonose@matera.com>
2016-04-17 17:48:50 -0300

    Revert board coordinates.

--- lib/tetris_board.ex
+++ lib/tetris_board.ex
@@ -2,11 +2,11 @@ defmodule TetrisBoard do
 
 	def get_new_map(height, width) do
 		bottom_row = 
-			0..width+1 |> Enum.map(fn x -> {0, x} end)
+			0..width+1 |> Enum.map(fn x -> {width, x} end)
 		left_column =
-			1..height |> Enum.map(fn x -> {x, 0} end)
+			0..height-1 |> Enum.map(fn x -> {x, 0} end)
 		right_column = 
-			1..height |> Enum.map(fn x -> {x, width+1} end)
+			0..height-1 |> Enum.map(fn x -> {x, width+1} end)
 		Enum.concat([bottom_row, left_column, right_column])
 	end
 
